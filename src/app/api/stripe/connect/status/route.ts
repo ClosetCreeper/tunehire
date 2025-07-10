d68c797b46f2199d1f4fdf95fa2b6ecbb68c637f
closetcreeper <95090991+ClosetCreeper@users.noreply.github.com>
2025-07-11 00:52:51 +0200

    Fixed Stripe API in other file

--- src/app/api/stripe/connect/status/route.ts
+++ src/app/api/stripe/connect/status/route.ts
@@ -5,7 +5,7 @@ import { prisma } from '@/lib/prisma'
 import Stripe from 'stripe'
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
-  apiVersion: '2024-12-18.acacia',
+  apiVersion: '2025-06-30.basil',
 })
 
 export async function GET() {
